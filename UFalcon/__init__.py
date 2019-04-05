0d71620f4f191e77b197ef65085654a244e24752
Jörg Herbel <joerg.herbel@phys.ethz.ch>
2019-04-05 14:20:16 +0200

    adjusted __init__

--- UFalcon/__init__.py
+++ UFalcon/__init__.py
@@ -1,4 +1,7 @@
+import pkg_resources
+from . import lensing_weights, lightcone, shells, utils
+
 __author__ = 'Raphael Sgier'
 __email__ = 'rsgier@phys.ethz.ch'
-__version__ = '0.1.0'
 __credits__ = 'ETH Zurich, Institute for Particle Physics and Astrophysics'
+__version__ = pkg_resources.require(__package__)[0].version
